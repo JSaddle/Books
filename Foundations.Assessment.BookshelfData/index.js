884d270254ac88cc7243f4a9146e363528f56da9
Jacob Saddler <jacobasaddler@gmail.com>
2022-09-12 16:45:11 -0500

    Still figuring Srch Button

--- Foundations.Assessment.BookshelfData/index.js
+++ Foundations.Assessment.BookshelfData/index.js
@@ -19,12 +19,39 @@ const txtSrch = document.querySelector("#txtSrch");
 const srchBtn = document.querySelector("#srch-btn");
 srchBtn.addEventListener("click", Search);
 
-function Search() {
-    const txtInp = txtSrch.value;
-    const result = bookshelf.books.filter((element) =>
-      (String(element).toLowerCase().includes(txtInp))
-      );
-       console.log(result);
+// function Search() {
+//     const txtInp = txtSrch.value.toLowerCase();
+//     for (i = 0; i < bookshelf.length; i++) { 
+//       if (!bookshelf[i].innerHTML.toLowerCase().includes(txtInp)) {
+//           bookshelf[i].style.display="none";
+//       }
+//       else {
+//           bookshelf[i].style.display="li";                 
+//       }
+//   }
+
+
+
+// const newBookFilter = bookData.filter( x => { //DONT NEED IF/ELSE, just search for what includes
+//   //
+//   return .author.includes(txtinp) || x.subject.includes(txtinp))
+  
+//   Then ...
+  
+//   for (const bookInfo of newBookFilter){
+//   const book = newBook(bookinfo.author..lang....sub..)
+//   const newBookShelf = new Bookshelf()
+//   const newbookshelf1 = newbookshelf.addBook(book)
+//   app.append(newBookFilter.render())
+
+
+
+
+    // const result = bookshelf.books.filter((element) =>
+    //   (String(element).toLowerCase().includes(txtInp))
+    //   );
+    //    console.log(result);
+
   //  convert object to string
     
 ///////////Below are different methods I attempted to make my search bar work. I'm able to make an array show up in the console but the search bar is evidently not functional. ////////////////////  
@@ -40,22 +67,13 @@ function Search() {
   //       }
   //   }
   //   const fResult = bookshelf.books.filter(result);
-    // console.log(fResult);
-    //log result variable to make sure filter working properly
+  //   console.log(fResult);
+  //   log result variable to make sure filter working properly
     
 
-// const result = bookshelf.books.filter(checkC);
 
-// function checkC(word){
-//   if (String(word).toLowerCase().includes(txtInp)){
-//     return true;
-//   }
-//   else {
-//     return null;
-//   }
-// }
+
 
 // console.log(result);
 
 
-}
